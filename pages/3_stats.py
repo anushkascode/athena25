0d7d83fdccf8790900785e3efa34824b85dfc91c
anushkascode <anushkarane2018@gmail.com>
2025-03-02 04:00:02 -0800

    dbcskua

---
+++ pages/3_stats.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from PIL import Image 
+import pandas as pd
+
+st.title("Page 3: Data Analytics")
+# zip = st.number_input("What is your zipcode?")
+
+sdr_data = pd.read_csv("data/sdr_data.csv")
+st.scatter_chart(sdr_data)
+
+urban_data = pd.read_csv("data/global_urbanization_climate_metrics.csv")
+
+# data3 = pd.read_csv("data3.csv")
