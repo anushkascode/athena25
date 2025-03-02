7048fa493d5d1bf39f8396255004a6ead90919f7
PolukSharma <poluk.sharma@gmail.com>
2025-03-02 02:45:57 -0800

    Sticker Prettiness
    Replaced Images with Custom Stickers and updated messages
    
    Co-Authored-By: ndreahsu <201402276+ndreahsu@users.noreply.github.com>

--- pages/1_task.py
+++ pages/1_task.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image  # Import Pillow (PIL) for image handling
 
-def create_task(label, image_path, session_state_key):
+def create_task(label, image_path, session_state_key, message):
     if session_state_key not in st.session_state:
         st.session_state[session_state_key] = False
     
@@ -11,7 +11,7 @@ def create_task(label, image_path, session_state_key):
         st.session_state[session_state_key] = True
         try:
             Image.open(image_path)
-            st.toast("Congratulations! You earned a new sustainability sticker!")
+            st.toast("Congratulations! You earned a new sustainability sticker for " + message)
         except FileNotFoundError:
             st.error(f"Image not found: {image_path}")
     else:
@@ -19,5 +19,5 @@ def create_task(label, image_path, session_state_key):
 
 st.title("Page 1: Task Completion")
 
-create_task("Planted a tree", "static/tree.png","checkbox1_checked") 
-create_task("Purchase clothing secondhand", "static/thrift.png","checkbox2_checked") 
+create_task("Planted a tree", "static/actions-sticker.png","checkbox1_checked", "making a greener world!") 
+create_task("Purchase clothing secondhand", "static/shopping-sticker.png","checkbox2_checked", "reducing waste!") 
--- pages/2_scrapbook.py
+++ pages/2_scrapbook.py
@@ -3,12 +3,11 @@ from PIL import Image
 
 st.title("Page 2: Scrapbook")
 
-def show_sticker(label, image_link, message):
+def show_sticker(label, image_link):
     if label in st.session_state and st.session_state[label]:
         image = Image.open(image_link)
         st.image(image)
-        st.write(message)
 
 st.write("Complete sustainable tasks to earn stickers!")
-show_sticker("checkbox1_checked", "static/tree.png", "Tree Planted!")
-show_sticker("checkbox2_checked", "static/thrift.png", "Child Labor Avoided!")
+show_sticker("checkbox1_checked", "static/actions-sticker.png")
+show_sticker("checkbox2_checked", "static/shopping-sticker.png")
