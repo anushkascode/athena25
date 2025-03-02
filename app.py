2ab17f35fbd238eaa8bb49ed76cc1682abac4234
PolukSharma <poluk.sharma@gmail.com>
2025-03-01 17:46:03 -0800

    Basic Image Upload Setup
    upload image prompt when checkbox clicked, image displayed
    
    added secondary page, running commands to requirements

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
 st.write("Hello world")
+yes = st.checkbox("Planted a tree")
+
+if yes:
+    uploaded_file = st.file_uploader("Upload a picture of your task!", type=["jpg", "png", "jpeg"])
+    if uploaded_file is not None:
+        st.image(uploaded_file)
